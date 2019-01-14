ec5272d74bbd70480690a20aa9cd0c5c85b18094
Jonathan Wong <jonathan.wong110@gmail.com>
2019-01-14 15:15:45 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,7 @@
 # Write your #display_rainbow method here
 
 def display_rainbow(colors)
+<<<<<<< HEAD
   print "R: #{colors[0]}, "
   print "O: #{colors[1]}, "
   print "Y: #{colors[2]}, "
@@ -9,3 +10,13 @@ def display_rainbow(colors)
   print "I: #{colors[5]}, "
   puts "V: #{colors[6]}"
 end
+=======
+  puts colors[0]
+  puts colors[1]
+  puts colors[2]
+  puts colors[3]
+  puts colors[4]
+  puts colors[5]
+  puts colors[6]
+end
+>>>>>>> d0443cc44aea2f68762dc6b6e48e2c414bed2adf
